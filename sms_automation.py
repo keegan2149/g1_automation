613419a304eaf4acb18bfc8da43b3248ecebd3cc
Keegan Holley <keegan2149@gmail.com>
2020-06-13 18:20:12 -0400

    added methods and error handling for empty jobs screen

--- sms_automation.py
+++ sms_automation.py
@@ -1,28 +1,62 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait 
-
+import json
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-#options.binary_location = "/usr/bin/chromium"
-driver = webdriver.Chrome(executable_path='./chromedriver',chrome_options=options)
-driver.get('https://www.politicalcomms.com/users/sign_in')
-driver.find_element_by_id('user_email').send_keys('keegan2149@gmail.com')
-driver.find_element_by_id ('user_password').send_keys('G1consulting')
-driver.find_element_by_name('commit').submit()
-driver.find_element_by_name('accepted').click()
-driver.find_element_by_name('commit').submit()
-driver.find_element_by_link_text("/sms/425/send_message").click()
+#reads username and password and stores in a dictionary credentials[email:"your@wmail.com",password:"password"
+
+def get_creds(): 
+  cred_file = open("./credentials.json", "r")
+  credentials = cred_file.read()
+  credentials = json.loads(credentials)
+  cred_file.close()
+  return credentials   
 
 
-for x in range(3000):
-   driver.find_element_by_name('commit').submit()
-   WebDriverWait(driver, 3)
+def open_driver(options=['--ignore-certificate-errors',"--test-type"],driver_path='./chromedriver'):
+  driver_options = webdriver.ChromeOptions()
+  for x in options:
+  	driver_options.add_argument(x)
 
-driver.quit()
+  driver = webdriver.Chrome(executable_path=driver_path,chrome_options=driver_options)
+  return driver 
 
-driver.find_element_by_name('commit').click()
+
+def send_sms(credentials,driver,start_url='https://www.politicalcomms.com/users/sign_in',message_count=3000):
+  driver.get(start_url)
+  #login
+  driver.find_element_by_id('user_email').send_keys(credentials['email'])
+  driver.find_element_by_id ('user_password').send_keys(credentials['password'])
+  driver.find_element_by_name('commit').submit()
+
+  #Accept TOS
+  driver.find_element_by_name('accepted').click()
+  driver.find_element_by_name('commit').submit()
+
+  #Open SMS Project
+  #if there are multiple projects then one can be found by name or element id
+  try:
+    driver.find_element_by_link_text("/sms/425/send_message").click()  
+  except Exception as e:
+    print("no jobs found")
+    driver.stop_client()
+    driver.quit()
 
 
+  #Send message_count messages
+
+  for x in range(message_count):
+    driver.find_element_by_name('commit').submit()
+    WebDriverWait(driver, 3)
+
+  #close window
+  driver.quit()
+
+
+credentials = get_creds()
+driver = open_driver()
+send_sms(credentials,driver)
+
+#cool overloaded function call
+
+#send_sms(get_creds(),open_driver())
